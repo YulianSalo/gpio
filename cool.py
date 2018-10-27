104a8954e0320ecc47a02679db0110f2512ac5a5
YulianSalo <39466050+YulianSalo@users.noreply.github.com>
2018-10-27 22:19:11 +0300

    Minor code update and file name update suggestion

--- cool.py
+++ cool.py
@@ -30,49 +30,53 @@ def off ():
     GPIO.output(13, False)
     return 0
 
-try:
+if __name__ == '__main__':
+    try:
     state = off()
+    
     while True:
         fo = open("/sys/class/thermal/thermal_zone0/temp")
         str = fo.read()
         fo.close()
         print(str)
-        temp = int(str)/1000
+        temp = int(st
+                   r)/1000
         print(temp)
         if state == 0:
             if temp >= midtemp:
                 state = lowon()
+                
         if state == 1:
             if temp >= hightemp:
                 state = highon()
             else:
                 if temp <= midtemp - delta:
                     state = off ()
+                    
         if state == 2:
             if temp <= hightemp - delta:
                 state = lowon()
         print(state)
-        time.sleep(15)
-
-
-#try:
+        
+    time.sleep(15)
+    #try:
     #while True:
-        #char = screen.getch()
-        #print chr(char)
-        #if char == ord('q'):
-            #break
-        #elif char == ord('1'):
-            #GPIO.output(11, False)
-            #GPIO.output(13, False)
-        #elif char == ord('2'):
-            #GPIO.output(11, True)
-            #GPIO.output(13, False)
-        #elif char == ord('3'):
-            #GPIO.output(11, True)
-            #GPIO.output(13, True)
-        #time.sleep(.5)
+    #char = screen.getch()
+    #print chr(char)
+    #if char == ord('q'):
+        #break
+    #elif char == ord('1'):
+        #GPIO.output(11, False)
+        #GPIO.output(13, False)
+    #elif char == ord('2'):
+        #GPIO.output(11, True)
+        #GPIO.output(13, False)
+    #elif char == ord('3'):
+        #GPIO.output(11, True)
+        #GPIO.output(13, True)
+    #time.sleep(.5)
 
-finally:
-    curses.nocbreak(); screen.keypad(0); curses.echo()
-    curses.endwin()
-    GPIO.cleanup()
+    finally:
+        curses.nocbreak(); screen.keypad(0); curses.echo()
+        curses.endwin()
+        GPIO.cleanup()  
